73ed42b7a65756e757a5d64d8fc9e811847c3985
ylinwind <ylinwind@163.com>
2017-10-20 13:39:14 +0800

    加入动画元素

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       ]
     },
     devServer:{
-      // historyApiFallback: true,
+      historyApiFallback: true,
       inline: true,//注意：不写hot: true，否则浏览器无法自动更新；也不要写colors:true，progress:true等，webpack2.x已不支持这些
     },
     plugins:[
